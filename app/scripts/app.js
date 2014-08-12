7bf109bcd706fb9a806c439b7c6927b62509f121
Patrick O'Neill <patrick.oneill46@gmail.com>
2014-08-12 22:04:28 +0100

    Adding angular carousel

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'angularFileUpload'
+    'angularFileUpload',
+    'angular-carousel'
   ])
   .config(function ($routeProvider) {
     $routeProvider
