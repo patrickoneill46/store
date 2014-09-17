b3b20d40bafd97681abebdb42390354618a74449
Patrick O'Neill <patrick.oneill46@gmail.com>
2014-09-17 10:32:24 +0100

    Been away for a while - new commit

--- app/scripts/services/adminservice.js
+++ app/scripts/services/adminservice.js
@@ -11,7 +11,7 @@ angular.module('storeApp')
   .factory('adminService', ['$resource', function ($resource) {
     // Service logic
     // ...
-
+    console.log('something');
     // Public API here
     return {
 
@@ -23,6 +23,20 @@ angular.module('storeApp')
             },
             update: {
                 method: 'PUT'
+            },
+            remove: {
+                method: 'DELETE'
+            },
+            get: {
+                method: 'GET'
+            }
+        }),
+
+        image: $resource('/image/:imageId', {
+            imageId: 'imageId'
+        },{
+            add: {
+                method: 'POST'
             },
             remove: {
                 method: 'DELETE'
