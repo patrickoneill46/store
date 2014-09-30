3e7698d7878570e66447bb2035bec43dc8756025
Patrick O'Neill <patrick.oneill46@gmail.com>
2014-09-30 15:19:50 +0100

    Adding test for cart service

--- test/spec/services/cartservice.js
+++ test/spec/services/cartservice.js
@@ -6,13 +6,54 @@ describe('Service: cartService', function () {
   beforeEach(module('storeApp'));
 
   // instantiate service
-  var cartService;
-  beforeEach(inject(function (_cartService_) {
+  var cartService,
+      scope,
+      firstItem = {
+        _id: 'firstKey',
+        price: 100,
+        productName: 'Test product',
+        category: 'Starter'
+      },
+      secondItem = {
+          _id: 'secondKey',
+          price: 101,
+          productName: 'Test product',
+          category: 'Main'
+      };
+
+  beforeEach(inject(function ($injector, _cartService_) {
     cartService = _cartService_;
+      scope = $injector.get('$rootScope');
   }));
 
-  it('should do something', function () {
-    expect(!!cartService).toBe(true);
-  });
+
+    it('should expose the correct set of public methods', function(){
+
+        expect(typeof cartService.addItem).toBe('function');
+        expect(typeof cartService.removeItem).toBe('function');
+        expect(typeof cartService.incrementItem).toBe('function');
+        expect(typeof cartService.decrementItem).toBe('function');
+    });
+
+    it('should add and remove the basket to the root scope', function(){
+
+        cartService.addItem(firstItem);
+        cartService.addItem(secondItem, 3);
+
+        expect(scope.cartContents[firstItem._id].quantity).toBe(1);
+        expect(scope.cartContents[secondItem._id].quantity).toBe(3);
+
+        cartService.addItem(secondItem);
+        expect(scope.cartContents[secondItem._id].quantity).toBe(4);
+
+        cartService.decrementItem(firstItem._id);
+        expect(scope.cartContents[firstItem._id]).toBeNull();
+
+        cartService.decrementItem(secondItem._id);
+        expect(scope.cartContents[secondItem._id].quantity).toBe(3);
+
+        cartService.incrementItem(secondItem._id, 2);
+        expect(scope.cartContents[secondItem._id].quantity).toBe(5);
+    });
 
 });
