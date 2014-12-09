bd941e3da7a468aec864b676c11f02e8d126bbbb
Patrick O'Neill <patrick.oneill@thebioagency.com>
2014-12-09 22:18:32 +0000

    Adding order directive

---
+++ app/scripts/directives/order.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name storeApp.directive:order
+ * @description
+ * # order
+ */
+angular.module('storeApp')
+  .directive('order', function () {
+    return {
+      templateUrl: 'views/components/order.html',
+      restrict: 'E',
+      link: function postLink() {
+
+
+      	
+      }
+    };
+  });
