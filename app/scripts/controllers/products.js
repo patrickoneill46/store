912fa958cd7c20514621c53cf5f11ac2a2633dec
Patrick O'Neill <patrick.oneill@thebioagency.com>
2014-12-09 22:52:50 +0000

    Adding filter to product page

--- app/scripts/controllers/products.js
+++ app/scripts/controllers/products.js
@@ -10,18 +10,31 @@
 angular.module('storeApp')
   .controller('ProductsCtrl', ['$scope', 'productsService', 'cartService', function ($scope, productsService, cartService) {
 
+    //ToDo: Get from backend
+    $scope.categories = [
+        'Starters',
+        'Main',
+        'Dessert',
+        'Drinks'
+    ];
+
+    $scope.search = {
+    };
 
     function getProducts(){
-
 
         productsService.get(function(response){
 
             $scope.products = response.products;
             $scope.pageCount = response.pageCount;
             $scope.totalCount = response.totalCount;
-        })
+        });
 
     }
+
+    $scope.clearSearch = function(key){
+        delete $scope.search[key];
+    };
 
     $scope.addItem = function(item) {
         cartService.addItem(item, 1);
